a7474478bc8e812da160528e1c386b2b56e4d006
gurdl0525 <gurdl7011@dsm.hs.kr>
2023-08-28 21:12:11 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/highthonbackofficeserver/HighthonBackofficeServerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.highthonbackofficeserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HighthonBackofficeServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<HighthonBackofficeServerApplication>(*args)
+}

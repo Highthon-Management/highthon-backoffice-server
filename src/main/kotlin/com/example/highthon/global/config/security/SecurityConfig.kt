cebc2eccd22dd6f409195767df0ea44d5839ab6f
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-09-04 16:37:12 +0900

    📝 :: SecurityConfig

---
+++ src/main/kotlin/com/example/highthon/global/config/security/SecurityConfig.kt
@@ -0,0 +1,39 @@
+package com.example.highthon.global.config.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig(
+) {
+
+    @Bean
+    @Throws(Exception::class)
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        return http
+            .csrf().disable()
+            .formLogin().disable()
+            .cors()
+
+            .and()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+            .and()
+            .authorizeRequests()
+
+            .antMatchers().permitAll()
+            .anyRequest().permitAll()
+
+            .and().build()
+    }
+
+    @Bean
+    fun passwordEncoder() = BCryptPasswordEncoder()
+}

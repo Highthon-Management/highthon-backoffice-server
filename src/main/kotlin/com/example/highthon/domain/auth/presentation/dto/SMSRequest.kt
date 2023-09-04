7eecc5305cc9b0a946342906e12378e34eb579fe
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-09-04 18:58:09 +0900

    ⭐️ :: SMSRequest

---
+++ src/main/kotlin/com/example/highthon/domain/auth/presentation/dto/SMSRequest.kt
@@ -0,0 +1,13 @@
+package com.example.highthon.domain.auth.presentation.dto
+
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Pattern
+
+data class SMSRequest(
+    @field:NotNull(message = "null이 될 수 없습니다.")
+    @field:Pattern(
+        regexp = "^010\\d{4}\\d{4}\$",
+        message = "전화번호는 숫자만 넣어야 합니다.\nex) 01012345678"
+    )
+    val phoneNumber: String?
+)

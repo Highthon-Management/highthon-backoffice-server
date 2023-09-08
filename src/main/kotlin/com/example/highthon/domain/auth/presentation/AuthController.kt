c135c14d9f4e44956811ee1c1478a5d4f5e8ca3f
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-09-08 22:50:47 +0900

    ⭐️ :: Login

--- src/main/kotlin/com/example/highthon/domain/auth/presentation/AuthController.kt
+++ src/main/kotlin/com/example/highthon/domain/auth/presentation/AuthController.kt
@@ -1,14 +1,20 @@
 package com.example.highthon.domain.auth.presentation
 
+import com.example.highthon.domain.auth.presentation.dto.SignUpRequest
 import com.example.highthon.domain.auth.presentation.dto.CertificateNumberRequest
+import com.example.highthon.domain.auth.presentation.dto.LoginRequest
 import com.example.highthon.domain.auth.presentation.dto.SMSRequest
+import com.example.highthon.domain.auth.presentation.dto.TokenResponse
+import com.example.highthon.domain.auth.service.AuthService
 import com.example.highthon.domain.auth.service.SMSService
 import net.nurigo.sdk.message.response.SingleMessageSentResponse
+import org.springframework.http.HttpStatus
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import javax.validation.Valid
 
@@ -16,7 +22,8 @@ import javax.validation.Valid
 @RequestMapping("/auth")
 @Validated
 class AuthController(
-    private val smsService: SMSService
+    private val smsService: SMSService,
+    private val authService: AuthService
 ) {
 
     @PostMapping("/sms")
@@ -34,4 +41,11 @@ class AuthController(
     ): Boolean {
         return smsService.certificateNumber(req)
     }
+
+    @PostMapping("/login")
+    @ResponseStatus(HttpStatus.CREATED)
+    fun login(
+        @RequestBody @Valid
+        req: LoginRequest
+    ): TokenResponse = authService.login(req)
 }

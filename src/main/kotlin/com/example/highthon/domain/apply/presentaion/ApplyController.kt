b6a1e8a2420dbf06e6ba094ce775c756f1360d32
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-09-11 10:34:52 +0900

    ⭐️ :: Apply

---
+++ src/main/kotlin/com/example/highthon/domain/apply/presentaion/ApplyController.kt
@@ -0,0 +1,28 @@
+package com.example.highthon.domain.apply.presentaion
+
+import com.example.highthon.domain.apply.presentaion.dto.request.ApplyRequest
+import com.example.highthon.domain.apply.presentaion.dto.response.ApplyResponse
+import com.example.highthon.domain.apply.service.ApplyService
+import org.springframework.http.HttpStatus
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
+
+@Validated
+@RestController
+@RequestMapping("/apply")
+class ApplyController(
+    private val applyService: ApplyService
+) {
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    fun apply(
+        @RequestBody @Valid
+        req: ApplyRequest
+    ): ApplyResponse = applyService.applyMe(req)
+}

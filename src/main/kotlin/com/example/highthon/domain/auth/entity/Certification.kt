bac4eb74725542f7a1c3aae74f7bb5bd140e529c
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-09-11 15:30:45 +0900

    ⭐️ :: 메세지에 타입 부여

--- src/main/kotlin/com/example/highthon/domain/auth/entity/Certification.kt
+++ src/main/kotlin/com/example/highthon/domain/auth/entity/Certification.kt
@@ -1,15 +1,22 @@
 package com.example.highthon.domain.auth.entity
 
+import com.example.highthon.domain.auth.entity.type.NumberType
 import org.springframework.data.annotation.Id
 import org.springframework.data.redis.core.RedisHash
 import org.springframework.data.redis.core.index.Indexed
+import javax.persistence.EnumType
+import javax.persistence.Enumerated
 
 @RedisHash(timeToLive = 300) // 5분
 data class Certification(
 
     @Id
-    var requester: String,
+    var id: String,
 
     @Indexed
-    var number: Int
+    var number: Int,
+
+    @Indexed
+    @Enumerated(EnumType.STRING)
+    var type: NumberType
 )

b24c37668b142f7439f6de5186575156ef5d9b3a
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-09-11 16:23:36 +0900

    ⭐️ :: editProfile

--- src/main/kotlin/com/example/highthon/domain/user/presentation/UserController.kt
+++ src/main/kotlin/com/example/highthon/domain/user/presentation/UserController.kt
@@ -1,8 +1,11 @@
 package com.example.highthon.domain.user.presentation
 
+import com.example.highthon.domain.user.presentation.dto.request.EditProfileRequest
 import com.example.highthon.domain.user.presentation.dto.response.UserProfileResponse
 import com.example.highthon.domain.user.service.UserService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -14,4 +17,7 @@ class UserController(
 
     @GetMapping("/me")
     fun getProfile(): UserProfileResponse = userService.getProfile()
+
+    @PutMapping("/edit")
+    fun editProfile(req: EditProfileRequest): UserProfileResponse = userService.editProfile(req)
 }

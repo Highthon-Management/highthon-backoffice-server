032dff1a27b216387dc43d4d76b289e1c2948974
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-09-11 10:41:53 +0900

    🔥 :: part삭제

--- src/main/kotlin/com/example/highthon/domain/user/entity/User.kt
+++ src/main/kotlin/com/example/highthon/domain/user/entity/User.kt
@@ -1,13 +1,10 @@
 package com.example.highthon.domain.user.entity
 
-import com.example.highthon.domain.user.entity.type.Part
 import com.example.highthon.domain.user.entity.type.Role
 import com.example.highthon.domain.user.presentation.dto.response.UserProfileResponse
 import org.hibernate.annotations.DynamicUpdate
 import java.util.*
 import javax.persistence.*
-import javax.validation.constraints.Max
-import javax.validation.constraints.Min
 
 @Entity(name = "user")
 @DynamicUpdate
@@ -17,7 +14,6 @@ class User(
     name: String,
     phoneNumber: String,
     school: String,
-    part: Part,
     password: String,
     grade: Int,
     role: Role
@@ -43,23 +39,16 @@ class User(
     @Column(name = "school", columnDefinition = "VARCHAR(15)", nullable = false)
     var school: String = school
 
-    @Column(name = "part", columnDefinition = "VARCHAR(9)", nullable = false)
-    @Enumerated(EnumType.STRING)
-    var part: Part = part
-        protected set
-
     @Column(name = "password", columnDefinition = "CHAR(60)", nullable = false)
     var password: String = password
         protected set
 
-    @Min(1)
-    @Max(3)
     @Column(name = "grade", nullable = false)
     var grade: Int = grade
         protected set
 
     @Column(name = "role", columnDefinition = "VARCHAR(11)", nullable = false)
-    @Enumerated(EnumType.STRING)
+    @Enumerated(value = EnumType.STRING)
     var role: Role = role
         protected set
 
@@ -67,7 +56,6 @@ class User(
         this.id,
         this.name,
         this.grade,
-        this.part,
         this.phoneNumber,
         this.school
     )

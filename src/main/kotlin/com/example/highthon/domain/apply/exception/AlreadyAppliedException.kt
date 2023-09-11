fdd62f0744b14c8115b264004dfc725665044bf9
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-09-11 10:34:13 +0900

    ⭐️ :: AlreadyAppliedException

---
+++ src/main/kotlin/com/example/highthon/domain/apply/exception/AlreadyAppliedException.kt
@@ -0,0 +1,6 @@
+package com.example.highthon.domain.apply.exception
+
+import com.example.highthon.global.config.error.data.ErrorCode
+import com.example.highthon.global.config.error.exception.BusinessException
+
+object AlreadyAppliedException: BusinessException(ErrorCode.ALREADY_APPLIED)

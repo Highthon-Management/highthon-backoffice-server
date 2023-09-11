6508cd874560ae548557b15027d1ce4e82cf9992
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-09-11 10:37:38 +0900

    🚚 :: global -> global.facade

--- src/main/kotlin/com/example/highthon/domain/user/service/UserServiceImpl.kt
+++ src/main/kotlin/com/example/highthon/domain/user/service/UserServiceImpl.kt
@@ -1,7 +1,7 @@
 package com.example.highthon.domain.user.service
 
 import com.example.highthon.domain.user.presentation.dto.response.UserProfileResponse
-import com.example.highthon.global.facade.UserFacade
+import com.example.highthon.global.common.facade.UserFacade
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 

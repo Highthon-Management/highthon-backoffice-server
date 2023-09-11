94a3b30dcaf4bc056a8bdfc4c4acac4032c61c71
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-09-11 17:32:01 +0900

    ⭐️ :: editApply

--- src/main/kotlin/com/example/highthon/domain/apply/presentaion/ApplyController.kt
+++ src/main/kotlin/com/example/highthon/domain/apply/presentaion/ApplyController.kt
@@ -6,6 +6,7 @@ import com.example.highthon.domain.apply.service.ApplyService
 import org.springframework.http.HttpStatus
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
@@ -25,4 +26,10 @@ class ApplyController(
         @RequestBody @Valid
         req: ApplyRequest
     ): ApplyResponse = applyService.apply(req)
+
+    @PutMapping
+    fun edit(
+        @RequestBody @Valid
+        req: ApplyRequest
+    ): ApplyResponse = applyService.edit(req)
 }

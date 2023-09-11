a6d81456f69d15d7f5aef5fdf38b95bfede48d53
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-09-11 10:42:30 +0900

    🔥 :: part삭제

--- src/main/kotlin/com/example/highthon/domain/user/presentation/dto/response/UserProfileResponse.kt
+++ src/main/kotlin/com/example/highthon/domain/user/presentation/dto/response/UserProfileResponse.kt
@@ -1,13 +1,11 @@
 package com.example.highthon.domain.user.presentation.dto.response
 
-import com.example.highthon.domain.user.entity.type.Part
-import java.util.UUID
+import java.util.*
 
 data class UserProfileResponse(
     val id: UUID,
     val name: String,
     val grade: Int,
-    val part: Part,
     val phoneNumber: String,
     val school: String
 )

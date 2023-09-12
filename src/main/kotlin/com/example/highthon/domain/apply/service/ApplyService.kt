593d04d5b6b529d68a8b482e427482a562c47b7e
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-09-12 13:21:54 +0900

    ⭐️ :: Apply 자세히 보기

--- src/main/kotlin/com/example/highthon/domain/apply/service/ApplyService.kt
+++ src/main/kotlin/com/example/highthon/domain/apply/service/ApplyService.kt
@@ -2,6 +2,7 @@ package com.example.highthon.domain.apply.service
 
 import com.example.highthon.domain.apply.presentaion.dto.request.ApplyRequest
 import com.example.highthon.domain.apply.presentaion.dto.response.ApplyResponse
+import java.util.UUID
 
 interface ApplyService {
 
@@ -10,4 +11,6 @@ interface ApplyService {
     fun edit(req: ApplyRequest): ApplyResponse
 
     fun cancel(reason: String)
+
+    fun getDetail(id: UUID): ApplyResponse
 }

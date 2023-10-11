3be60ec5501e7d1ce889529a41fb5308c302df08
gurdl7011 <gurdl7011@dsm.hs.kr>
2023-10-11 14:02:55 +0900

    🔥 :: findAllBy*

--- src/main/kotlin/com/example/highthon/domain/apply/repository/ApplicantRepository.kt
+++ src/main/kotlin/com/example/highthon/domain/apply/repository/ApplicantRepository.kt
@@ -1,10 +1,6 @@
 package com.example.highthon.domain.apply.repository
 
 import com.example.highthon.domain.apply.entity.Applicant
-import com.example.highthon.domain.user.entity.type.Part
-import com.example.highthon.domain.user.entity.type.Role
-import org.springframework.data.domain.Page
-import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import java.util.*
@@ -12,7 +8,4 @@ import java.util.*
 @Repository
 interface ApplicantRepository: JpaRepository<Applicant, UUID> {
 
-    fun findAllByAndUserPartAndIsCanceledAndUserRole(part: Part, isCanceled: Boolean, userRole: Role, pageable: Pageable): Page<Applicant>
-
-    fun findAllByIsCanceledAndUserRole(isCanceled: Boolean, userRole: Role, pageable: Pageable): Page<Applicant>
 }
